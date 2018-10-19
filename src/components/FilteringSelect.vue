35b8ba2a93007e0cecb30670dce10dc099c703e3
mattdiez <mattdiez@gmail.com>
2018-10-19 15:56:49 -0500

    Add files via upload

--- src/components/FilteringSelect.vue
+++ src/components/FilteringSelect.vue
@@ -1,4 +1,5 @@
 <template>
+	<!-- TODO: paging of results (keys?) -->
     <div>        
         <!-- for debugging purposes -->		
         <div class="input-group-placeholder"  v-bind:class="{ expanded : renderSuggestions, 'is-invalid' : hasError}">        	
@@ -11,7 +12,9 @@
                 tabindex="-1"/>
 			
             <!-- //form-control text-input    -->
-            <input :disabled="disabled" @click="emitClickInput" 
+            <input 
+            	:disabled="disabled" 
+            	@click="emitClickInput" 
                 @keydown="keyDownListener"                
                 @focus="focusListener"
                 @blur="blurListener"
@@ -23,6 +26,7 @@
                 placeholder="" 
                 autocomplete="off" 
                 :required="required"
+                :tabindex="tabindex"
                 class="form-control text-input"                                
                 v-bind:class="{'is-invalid' : hasError}"
                 />
@@ -82,7 +86,8 @@ export default {
 			loadingResponse: false,
 			canSend: true,
 			typingForward: true,
-			selectedItem: null			
+			selectedValue: this.value, 
+			selectedItem: null	
 		}
   	}, 
 	props: { // these are passed in
@@ -97,11 +102,7 @@ export default {
 		value: {
 			type: [Object, String],
 			default: () => {}
-		},
-		valueItem: {
-			type: [Object, String],
-			default: () => {}
-		},				
+		},					
 		valueAttr: {
 		    type: String,
 		    default: 'key'
@@ -145,18 +146,13 @@ export default {
 		simpleSelect: {
 			type: Boolean,
 			default: false
+		},
+		tabindex: {
+			type: Number,
+			default: 0
 		}
  	}, 
 	computed: {		
-		rawValue: {
-			get() {
-				return this.value;
-			},
-			set(newValue) {
-				this.$emit('input', newValue)
-				this.$emit('select', newValue)
-			}		
-		},		
 		showSuggestions: function() {
 			return  this.suggestions.length > 0 || this.loadingResponse;
 		},
@@ -180,230 +176,246 @@ export default {
 		},    
 	},
 	watch: {
-		selectedItem(newVal) {
-			this.rawValue = newVal ? newVal[this.valueAttr] : null;			
+		value() {
+			// watch changes in the v-model property			
+			if ((!this.listIsRequest()) && (this.value)) {
+				// TODO: This is called in created() AND here, maybe merge
+  				this.select(this.list.find(item => this.valueProperty(item) === this.value))  				
+  			} else {
+  				this.selectedValue = this.value;
+  			}
 		},
+		selectedItem(newItem) {
+			// watch changes on suggestion select/click
+			this.selectedValue = newItem ? newItem[this.valueAttr] : null;			
+		},
+		selectedValue(newValue) {
+			// trigger 'input' event to update v-model
+			this.$emit('input', this.selectedValue)
+		}
 	},
-  methods: {
-	emitClickInput () {
-		// TODO: implement this
-	},
-	keyDownListener (event) {
-		this.typingForward = true;
+	methods: {
+		emitClickInput () {
+			// TODO: implement this
+		},
+		keyDownListener (event) {
+			this.typingForward = true;
 
-		if (['Delete', 'Backspace'].indexOf(event.code) != -1) {
-			this.clearSelection();
-			this.typingForward = false;
-		}
-		
-		if (['NumpadEnter', 'Enter'].indexOf(event.code) != -1) {			
-			if ((this.renderSuggestions) && (this.suggestions.length > 0) ){								
-				event.preventDefault();
-				this.select(this.suggestions[this.hoveredIndex != -1 ? this.hoveredIndex : 0]);				
-			}
-
-		}
-		
-		this.moveSelection(event);
-	},
-	inputListener() {		
-		if (this.selectedItem) {				
-			this.clearSelection();
-		}
-		if (this.debounce) {
-			clearTimeout(this.timeoutInstance)
-			this.timeoutInstance = setTimeout(this.research, this.debounce)
-		} else {
-			this.research()
-		}	
-	},
-    async research () {
-      try {
-        if (this.canSend) {
-          this.canSend = false
-          this.$set(this, 'suggestions', await this.getSuggestions())
-        }
-      }
-      catch (e) {
-        this.clearSuggestions()
-        throw e
-      }
-      finally {
-        this.canSend = true
-				
-		this.showList();
-		
-		// Autoselect first/only item
-		if ((this.suggestions.length === 1) && (this.typingForward) && this.autoselectSingle) {
-			this.select(this.suggestions[0])
-		}
-        return this.suggestions
-      }
-    },
-	async focusListener () {
-		if (this.suggestions.length === 0) {
-			await this.research()
-		}
-		this.showList()		
-	},
-	hoverList (isOverList) {
-		this.isOverList = isOverList
-	},      
-	blurListener () {
-		if (!this.isOverList) {
-			if (!this.selectedItem) {
-				this.textInput = null;
-				this.clearSuggestions();
+			if (['Delete', 'Backspace'].indexOf(event.code) != -1) {
 				this.clearSelection();
-			}							
-			this.hideList();
-		}
-	}, 
-	async getSuggestions () {
-		let matches = [];
-		let results = [];
-		let queryText = this.textInput;		
+				this.typingForward = false;
+			}
 		
-		if (queryText) {
-			if ((queryText) && (queryText.length < this.minLength)) {
-          		this.hideList()
-				return []
-			}
-
-			this.clearSuggestions();
-
-			if (this.listIsRequest()) {
-				this.loadingResponse = true;
-				this.showList(); 
-				
-				results =  await (this.list(queryText)) || []
-				
-				this.loadingResponse = false;				
-			} else {
-				results = this.list;
-			}	
-
-			/*	
-			Note: This is doing a "startsWith" instead of an "indexOf"	
-			matches = results.filter(e => 
-				e[this.labelAttr].toLowerCase().startsWith(queryText.toLowerCase()) 
-			);
-			*/
-			matches = results.filter(e => 
-				e[this.labelAttr].toLowerCase().indexOf(queryText.toLowerCase()) != -1 
-			);			
-		} else {
-			// clear out selected item
-			this.clearSelection();
-			
-			if (!this.listIsRequest()) {
-				// not a request? just a list?
-				matches = this.list.filter(e => true);
-			}
-		}
-		// consider paging at some time
-		return matches.splice(0, this.maxResults);
-    },
-    highlightMatch (value) {
-        if (!value) return '-';
-
-		let result = value;
-		let query = this.textInput;
-
-		if (!query) return result;
-        
-		const texts = query.split(/[\s-_/\\|\.]/gm).filter(t => !!t) || [''];
-		return result.replace(new RegExp('(.*?)(' + texts.join('|') + ')(.*?)','gi'), '$1<b>$2</b>$3');
-    },
-	clearSelection () {		
-		this.selectedItem = null;	
-	},
-	showList () {
-		if (!this.renderSuggestions) {
-			if (
-				(this.textInput && (this.textInput.length >= this.minLength))
-				|| (this.simpleSelect)) {
-				this.renderSuggestions = true
-				this.$emit('show-list')
-        	}
-      	}
-	},
-	hideList () {
-		this.renderSuggestions = false;
-	},
-	clearSuggestions () {
-		this.suggestions = [];
-	},
-	suggestionClick (item) {
-		this.isOverList = false;
-		this.select(item);
-	},
-	select (item) {
-		this.selectedItem = item;
-		this.textInput = item[this.labelAttr];		
-
-		// reduce suggestion list to selected val
-		this.suggestions = [item];
-
-		this.hideList();
-	},
-	letterProcess (item) {
-		if (item[this.labelAttr].toLowerCase().startsWith(this.textInput.toLowerCase())) {
-			var remoteText = item[this.labelAttr].split('')
-			var inputText = this.textInput.split('')		
-			inputText.forEach(function (letter, key) {
-				if (letter !== remoteText[key]) {
-					remoteText[key] = letter
+			if (['NumpadEnter', 'Enter'].indexOf(event.code) != -1) {			
+				if ((this.renderSuggestions) && (this.suggestions.length > 0) ){								
+					event.preventDefault();
+					this.select(this.suggestions[this.hoveredIndex != -1 ? this.hoveredIndex : 0], true);				
 				}
-			})
-			return remoteText.join('')
-		}
-	},
-	moveSelection (e) {
-		
-		if ((e.code == 'ArrowDown') || (e.code == 'ArrowUp')) {
-			e.preventDefault()
-			this.showList()
-
-			const isMovingDown = e.code == 'ArrowDown';
-			const direction = isMovingDown * 2 - 1
-			const listEdge = isMovingDown ? 0 : this.suggestions.length - 1
-			const hoversBetweenEdges = isMovingDown ? this.hoveredIndex < this.suggestions.length - 1 : this.hoveredIndex > 0
-			
-			let item = null
-			
-			if (!this.hoveredItem) {
-				item = this.selectedItem || this.suggestions[listEdge]
-			} else if (hoversBetweenEdges) {
-				item = this.suggestions[this.hoveredIndex + direction]
-			} else /* if hovers on edge */ {
-				item = this.suggestions[listEdge]
 			}
-
-			this.hover(item)
-		}
-	},
-	hover (item, elem) {
-		this.hoveredItem = item
-		if (this.hovered != null) {
-			this.$emit('hover', item, elem)
-		}
-    },
-	listIsRequest () {
-		return typeof this.list === 'function'
-	},    
-    valueProperty (obj) {
-		//return this.getPropertyByAttribute(obj, this.valueAttribute)
-		return obj[this.valueAttr];
-    },
+		
+			this.moveSelection(event);
+		},
+		inputListener() {		
+			if (this.selectedItem) {				
+				this.clearSelection();
+			}
+			if (this.debounce) {
+				clearTimeout(this.timeoutInstance)
+				this.timeoutInstance = setTimeout(this.research, this.debounce)
+			} else {
+				this.research()
+			}	
+		},
+    	async research () {
+			try {
+        		if (this.canSend) {
+          			this.canSend = false
+          			this.$set(this, 'suggestions', await this.getSuggestions())
+        		}
+      		}
+      		catch (e) {
+        		this.clearSuggestions()
+        		throw e
+      		}
+      		finally {
+        		this.canSend = true
+				this.showList()
+		
+				// Autoselect first/only item
+				if ((this.suggestions.length === 1) && (this.typingForward) && this.autoselectSingle) {
+					this.select(this.suggestions[0], true)
+				}
+	        	return this.suggestions
+      		}
+    	},
+		async focusListener (event) {
+			// autoselect input
+			event.target.select();
+			if (this.suggestions.length === 0) {
+				await this.research()
+			}
+			this.showList()		
+		},
+		hoverList (isOverList) {
+			this.isOverList = isOverList
+		},      
+		blurListener () {
+			if (!this.isOverList) {
+				if (!this.selectedItem) {
+					this.textInput = null;
+					this.clearSuggestions();
+					this.clearSelection();
+				}							
+				this.hideList();
+			}
+		}, 
+		async getSuggestions () {
+			let matches = [];
+			let results = [];
+			let queryText = this.textInput;		
+			
+			if (queryText) {
+				if ((queryText) && (queryText.length < this.minLength)) {
+	          		this.hideList()
+					return []
+				}
 	
-	// paging of results (keys?)
-  },
-  mounted() {
-  	if ((!this.listIsRequest()) && (this.value)) {
-  		this.select(this.list.filter(item => this.valueProperty(item) === this.value)[0])
-  	}
-  }
+				this.clearSuggestions();
+	
+				if (this.listIsRequest()) {
+					this.loadingResponse = true;
+					this.showList(); 
+					
+					results =  await (this.list(queryText)) || []
+					
+					this.loadingResponse = false;				
+				} else {
+					results = this.list;
+				}	
+	
+				/*	
+				Note: This is doing a "startsWith" instead of an "indexOf"	
+				matches = results.filter(e => 
+					e[this.labelAttr].toLowerCase().startsWith(queryText.toLowerCase()) 
+				);
+				*/
+				matches = results.filter(e => 
+					e[this.labelAttr].toLowerCase().indexOf(queryText.toLowerCase()) != -1 
+				);			
+			} else {
+				// clear out selected item
+				this.clearSelection();
+				
+				if (!this.listIsRequest()) {
+					// not a request? just a list?
+					matches = this.list.filter(e => true);
+				}
+			}
+			// consider paging at some time
+			return matches.splice(0, this.maxResults);
+	    },
+	    highlightMatch (value) {
+	        if (!value) return '-';
+	
+			let result = value;
+			let query = this.textInput;
+	
+			if (!query) return result;
+	        
+			const texts = query.split(/[\s-_/\\|\.]/gm).filter(t => !!t) || [''];
+			return result.replace(new RegExp('(.*?)(' + texts.join('|') + ')(.*?)','gi'), '$1<b>$2</b>$3');
+	    },
+		clearSelection () {		
+			this.selectedItem = null;	
+		},
+		showList () {
+			if (!this.renderSuggestions) {
+				if (
+					(this.textInput && (this.textInput.length >= this.minLength))
+					|| (this.simpleSelect)) {
+					this.renderSuggestions = true
+					this.$emit('show-list')
+	        	}
+	      	}
+		},
+		hideList () {
+			this.renderSuggestions = false;
+		},
+		clearSuggestions () {
+			this.suggestions = [];
+		},
+		suggestionClick (item) {
+			this.isOverList = false;
+			this.select(item, true);
+		},
+		select (item, interactive = false) {
+			this.selectedItem = item;
+			this.textInput = item[this.labelAttr];		
+	
+			// reduce suggestion list to selected val
+			this.suggestions = [item];
+	
+			this.hideList();
+			if (interactive) {
+				this.$emit('select', this.selectedValue)
+			}
+		},
+		letterProcess (item) {
+			if (item[this.labelAttr].toLowerCase().startsWith(this.textInput.toLowerCase())) {
+				var remoteText = item[this.labelAttr].split('')
+				var inputText = this.textInput.split('')		
+				inputText.forEach(function (letter, key) {
+					if (letter !== remoteText[key]) {
+						remoteText[key] = letter
+					}
+				})
+				return remoteText.join('')
+			}
+		},
+		moveSelection (e) {
+			
+			if ((e.code == 'ArrowDown') || (e.code == 'ArrowUp')) {
+				e.preventDefault()
+				this.showList()
+	
+				const isMovingDown = e.code == 'ArrowDown';
+				const direction = isMovingDown * 2 - 1
+				const listEdge = isMovingDown ? 0 : this.suggestions.length - 1
+				const hoversBetweenEdges = isMovingDown ? this.hoveredIndex < this.suggestions.length - 1 : this.hoveredIndex > 0
+				
+				let item = null
+				
+				if (!this.hoveredItem) {
+					item = this.selectedItem || this.suggestions[listEdge]
+				} else if (hoversBetweenEdges) {
+					item = this.suggestions[this.hoveredIndex + direction]
+				} else /* if hovers on edge */ {
+					item = this.suggestions[listEdge]
+				}
+	
+				this.hover(item)
+			}
+		},
+		hover (item, elem) {
+			this.hoveredItem = item
+			if (this.hovered != null) {
+				this.$emit('hover', item, elem)
+			}
+	    },
+		listIsRequest () {
+			return typeof this.list === 'function'
+		},    
+	    valueProperty (obj) {
+			//return this.getPropertyByAttribute(obj, this.valueAttribute)
+			return obj[this.valueAttr];
+	    }
+		
+	},
+	created() {
+		if ((!this.listIsRequest()) && (this.value)) {
+			this.select(this.list.find(item => this.valueProperty(item) === this.value))
+		}
+	}
 }
 </script>
 
